a0c439bd8452d06255489bdf7c7e650c967b927f
Polina <pppzzz22229@gmail.com>
2018-12-10 10:14:37 +0300

    Class SignUp
    Sign up form for /sign_up

--- strategy_html_pages/app/forms.py
+++ strategy_html_pages/app/forms.py
@@ -3,7 +3,11 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators = [DataRequired()])
-	password = PasswordField('Password', validators = [DataRequired()])
-	remember_me = BooleanField('Remember Me')
-	submit = SubmitField('Sign In')
+    username = StringField('Username', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class SignUp(FlaskForm):
+    #TODO name, secondname, username, password, submit
+    
