04249210d095a5e63e5842e9c2a6ff735fbf69f3
Polina <pppzzz22229@gmail.com>
2018-12-15 23:32:28 +0300

    Create SignUp class
    name, secondname, username, password, remember me, submit - fields

--- strategy_html_pages/app/forms.py
+++ strategy_html_pages/app/forms.py
@@ -9,5 +9,10 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 class SignUp(FlaskForm):
+    name = StringField('Name', validators = [DataRequired()])
+    secondname = StringField('Second name', validators = [DataRequired()])
+    username = StringField('Username*', validators = [DataRequired()])
+    password = PasswordField('Password*', validators = [DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
     #TODO name, secondname, username, password, submit
-    
