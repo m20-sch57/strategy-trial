89b8ebf12c8941d5e6d95c24395763213919b391
cookiedoth <savkinsd2089@gmail.com>
2018-12-22 20:00:14 +0300

    I HAVE OPTIMIZED THIS CODE

--- judge.py
+++ judge.py
@@ -14,15 +14,14 @@ def importPath(path: str):
 import time
 import multiprocessing as mp
 
-def notTL(pool):
+def closePool(pool):
 	pool.close()
 	pool.join()
 
-def runStrategy(game, strategy, gameState, playerId: int, logs):
+def runStrategy(game, strategy, gameState, playerId: int, logs, pool):
 	partialGameState = game.gameStateRep(gameState, playerId)
 	result = [StrategyVerdict.Ok]
 
-	pool = mp.Pool(processes = 1)
 	func = pool.apply_async(strategy.Strategy, args = (partialGameState, playerId))
 
 	try:
@@ -31,10 +30,7 @@ def runStrategy(game, strategy, gameState, playerId: int, logs):
 		pool.terminate()
 		result[0] = [StrategyVerdict.TimeLimitExceeded]
 	except Exception:
-		notTL(pool)
 		result[0] = [StrategyVerdict.Failed]
-	else:
-		notTL(pool)
 
 	if (result[0] == StrategyVerdict.Ok):
 		result.append(turn)
@@ -54,6 +50,10 @@ def strategyFailResults(game, strategyId : int, verdict) -> list:
 	results[strategyId].verdict = verdict
 	return results
 
+def closePools(pools):
+	for pool in pools:
+		closePool(pool)
+
 def run(gamePath: str, classesPath: str, strategyPathes : list, saveLogs = False) -> InvocationResult:
 	classes = importPath(classesPath)
 	game = importPath(gamePath)
@@ -69,26 +69,32 @@ def run(gamePath: str, classesPath: str, strategyPathes : list, saveLogs = False
 	fullGameState = game.FullGameState()
 	whoseTurn = 0
 
+	pools = [mp.Pool(processes = 1) for i in range(PlayesCount)]
+
 	for i in range(game.TurnLimit):
-		turnList = runStrategy(game, strategies[whoseTurn], fullGameState, whoseTurn, logs)
+		turnList = runStrategy(game, strategies[whoseTurn], fullGameState, whoseTurn, logs, pools[whoseTurn])
 		if (turnList[0] != StrategyVerdict.Ok):
 			result.results = strategyFailResults(game, whoseTurn, turnList[0])
+			closePools(pools)
 			return result
 		
 		turnResult = game.makeTurn(fullGameState, whoseTurn, turnList[1], logs)
 		if (turnResult[0] == TurnState.Incorrect):
 			result.results = strategyFailResults(game, whoseTurn, StrategyVerdict.Incorrect)
 			logs.unexpectedVerdict(whoseTurn, StrategyVerdict.Incorrect)
+			closePools(pools)
 			return result
 
 		if (turnResult[0] == TurnState.Last):
 			result.results = turnResult[1]
+			closePools(pools)
 			return result
 
 		fullGameState = turnResult[1]
 		whoseTurn = turnResult[2]
 
 	result.results = [Result() for i in range(PlayesCount)]
+	closePools(pools)
 	return result
 
 if __name__ == '__main__':
