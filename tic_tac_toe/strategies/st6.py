d9889835450252550eefceae7da10b67476bef1a
Semyon Savkin <savkinsd2089@gmail.com>
2019-02-10 16:12:37 +0300

    converting

--- tic_tac_toe/strategies/st6.py
+++ tic_tac_toe/strategies/st6.py
@@ -3,5 +3,5 @@ from classes import *
 def Strategy(a: GameState, b: int) -> Turn:
     x = 56
     for i in range(1000000000):
-    	x = (x * 228) % 1337
+        x = (x * 228) % 1337
     return Turn(0, 0)
