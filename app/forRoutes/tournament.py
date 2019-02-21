52a81411ce172281c8e955185760536c3fb07c5e
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-02-22 00:08:12 +0300

    fixing bugs

--- app/forRoutes/tournament.py
+++ app/forRoutes/tournament.py
@@ -1,5 +1,6 @@
 from app.forms import TournamentForm
 import server.useCasesAPI as useCasesAPI
+from server.storage import storage
 
 def Tournament(form: TournamentForm) -> list:
     if form.validate_on_submit():
@@ -7,6 +8,8 @@ def Tournament(form: TournamentForm) -> list:
         id = None
         try:
             id = int(probId)
+            if storage.getProblem(id) == None:
+                raise ValueError()
         except ValueError:
             return [0, "No problem with this id!"]
         useCasesAPI.tournament(id)
