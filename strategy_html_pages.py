1b224ad0cb5b3039a5258393fd014beca526c65e
Semyon Savkin <savkinsd2089@gmail.com>
2019-04-23 21:45:40 +0500

    timer function

--- strategy_html_pages.py
+++ strategy_html_pages.py
@@ -1,6 +1,18 @@
 import sys
 sys.path.append("/")
 
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 from app import app
 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=1)
+scheduler.start()
+
 app.run(host = "0.0.0.0")
+
+atexit.register(lambda: scheduler.shutdown())
