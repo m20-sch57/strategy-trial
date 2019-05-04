610d304fcb16c74e3a9c015c600fdb6252ef9dbb
Semyon Savkin <savkinsd2089@gmail.com>
2019-05-05 01:01:27 +0300

    omg............

--- app/forRoutes/changeChatPage.py
+++ app/forRoutes/changeChatPage.py
@@ -1,6 +1,10 @@
 def getPageId(nextPageForm, prevPageForm, curPageId):
-	if (nextPageForm.validate_on_submit()):
+	print(nextPageForm.submit.data)
+	print(prevPageForm.submit.data)
+	if (nextPageForm.submit.data):
+		print("fuck")
 		return curPageId + 1
-	if (prevPageForm.validate_on_submit()):
+	if (prevPageForm.submit.data):
+		print("fuuck")
 		return curPageId - 1
 	return None
--- app/forRoutes/messagePost.py
+++ app/forRoutes/messagePost.py
@@ -2,7 +2,8 @@ import server.useCasesAPI as useCasesAPI
 from app.forRoutes.info import info
 
 def sendMessage(form, info):
-	if form.validate_on_submit():
+	if (form.validate_on_submit() and form.submit.data):
+		print("FCK")
 		useCasesAPI.createMessage(form.contentField.data, info)
 		return 1
 	else:
