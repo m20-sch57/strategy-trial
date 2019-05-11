d9b94c81cf219540131c7f2fd46bd9d01a41cb4d
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-05-11 19:37:41 +0300

    Strategy that gets always 'Failed'

---
+++ ticTacToeStrategies/st11.py
@@ -0,0 +1,10 @@
+from classes import *
+
+def check(a: GameState, i: int, j: int) -> bool:
+    return a.a[i][j] == '.'
+
+def Strategy(a: GameState, b: int) -> Turn:
+   while True:
+       i, j = random.randint(0, 2), random.randint(0, 2)
+       if check(a, i, j):
+           return Turn(i, j)
