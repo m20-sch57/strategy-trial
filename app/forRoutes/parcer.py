4cabe293582745f2d0ab2d33594bfa5ec760e387
Polina <pppzzz22229@gmail.com>
2019-05-12 22:26:23 +0300

    Add bold text

--- app/forRoutes/parcer.py
+++ app/forRoutes/parcer.py
@@ -1,9 +1,7 @@
-#<website.net> - Website page
+#[website.net] - Website page
 #__text__ - underlined text
+#**text** - bold text
 
-"""
-сначала орределение ссылок
-"""
 stringlong = 60
 linenumber = 0
 def anti(symbol):
@@ -18,17 +16,18 @@ def addText(text, i, symbol):
     if j != -1:
         if text[i:i+j+1].find('<') == -1:
             if symbol == '[':
-                add = '<a href="' + text[i+1:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
-                j += 1
+                add = '<a href="http://' + text[i+1:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
             if symbol == '__':
                 add = '<u>' + text[i+2:i+j+1] + '</u>'
-                j += 2
+                j += 1
+            if symbol == '**':
+                add = '<b>' + text[i+2:i+j+1] + '</b>'
+                j += 1
         else:
             add = '[SOMETHING IS WRONG]'
     else:
         add = text[i]
-        j = 0
-    return [add, i + j]
+    return [add, i + j + 1]
 
 def parcer(text):
     len_text, i = len(text), 0
@@ -38,16 +37,15 @@ def parcer(text):
             newtext += '"<"'
         elif text[i] == '>':
             newtext += '">"'
-        elif text[i] == '[' or (i + 1 < len_text and text[i] + text[i+1] == '__'):
+        elif text[i] == '[' or (i + 1 < len_text and (text[i] + text[i+1] in ['**', '__'])):
             symbol = text[i]
-            if text[i] == '_':
-                symbol += '_'
+            if text[i] in ['*', '_']:
+                symbol += text[i]
             list_n = addText(text, i, symbol)
             newtext += list_n[0]
             i = list_n[1]
         else:
             newtext += text[i]
         i += 1
-        print(newtext)
     return newtext
 
