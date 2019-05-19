5aacb14263be438c1f604acef8740cb01a06257c
Polina <pppzzz22229@gmail.com>
2019-05-19 23:55:59 +0300

    Done

--- app/forRoutes/sign_up.py
+++ app/forRoutes/sign_up.py
@@ -3,8 +3,8 @@ import server.structures as structures
 from app.forms import SignUp
 import re
 
-def validateUsername(username):
-    return (re.fullmatch('[0-9a-zA-Z._-]+', username) is not None)
+def validateName(name):
+    return (re.fullmatch('[0-9a-zA-Z._-]+', name) is not None)
 
 def Sign_up(form: SignUp) -> list:
     if form.validate_on_submit():
@@ -13,7 +13,11 @@ def Sign_up(form: SignUp) -> list:
         username = form.username.data
         password = form.password.data
         passwordRet = form.passwordRet.data
-        if (not validateUsername(username)):
+        if (not validateName(name)):
+            return [0, ("Name is not correct", 'message red')]
+        if (not validateName(secondname)):
+            return [0, ("Secondname is not correct", 'message red')]
+        if (not validateName(username)):
             return [0, ("Username is not correct", 'message red')]
         if storage.storage.getUserByName(username) != None:
             return [0, ("There is a user with this username", 'message red')]
