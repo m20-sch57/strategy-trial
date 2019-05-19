c9c364f3362e344ec9714ea29d53e2fa4dfed9e4
Polina <pppzzz22229@gmail.com>
2019-05-19 23:37:58 +0300

    chat2

--- app/forRoutes/parser.py
+++ app/forRoutes/parser.py
@@ -9,14 +9,17 @@ def anti(symbol):
     else:
         return symbol
 
-def addText(text, i, symbol, counter):
+def addText(text, i, symbol):
     add = ''
     j = text[i+1:].find(anti(symbol))
     if j != -1:
         if symbol == '[':
             if text[i+1:i+9] == 'https://':
-                i += 8
-            add = '<a href="https://' + text[i+1:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
+                add += '<a href="https://' + text[i+9:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
+            elif text[i+1:i+8] == 'http://':
+                add += '<a href="http://' + text[i+8:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
+            else:
+                add = '<a href="https://' + text[i+1:i+j+1] + '">' + text[i+1:i+j+1] + '</a>'
         if symbol == '__':
             add = '<u>' + text[i+2:i+j+1] + '</u>'
             j += 1
@@ -25,35 +28,25 @@ def addText(text, i, symbol, counter):
             j += 1
     else:
         add = text[i]
-    counter += j + 1
-    if counter >= stringlong:
-        counter, add = len(add), '\n' + add
     return [add, i + j + 1]
 
 def parser(text):
-    counter = 0
     len_text, i = len(text), 0
     newtext = ''
     while i < len_text:
         if text[i] == '<':
             newtext += '&lt;'
-            counter += 1
         elif text[i] == '>':
             newtext += '&gt;'
-            counter += 1
         elif text[i] == '[' or (i + 1 < len_text and (text[i] + text[i+1] in ['**', '__'])):
             symbol = text[i]
             if text[i] in ['*', '_']:
                 symbol += text[i]
-            list_n = addText(text, i, symbol, counter)
+            list_n = addText(text, i, symbol)
             newtext += list_n[0]
             i = list_n[1]
         else:
             newtext += text[i]
-            counter += 1
-        if counter >= stringlong:
-            newtext += '\n'
-            counter = 0
         i += 1
     return newtext
 
