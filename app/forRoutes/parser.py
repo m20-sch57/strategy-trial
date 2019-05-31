0b14a25b0e91cd00bcb6ffdcbcffa3744823f1c5
Polina <pppzzz22229@gmail.com>
2019-05-31 18:16:01 +0300

    Add some information

--- app/forRoutes/parser.py
+++ app/forRoutes/parser.py
@@ -13,12 +13,17 @@ def something_else_you_need(el):
     else:
         Link_text[0] += '</a>'
 
+#ocb - open (o) symbol, close (c) or both (b)
+#bool - True: in, False: out
+#anti - close symbol to open one and vice versa
+#num - number of symbols: '*': { ..,'num': 3} => '***'
+
 el_d = {
-'_': {'ocb': 'b', 'mean': ['<u>', '</u>'], 'anti': '_', 'bool': False, 'f': ['_', 2]}, 
-'*': {'ocb': 'b', 'mean': ['<b>', '</b>'], 'anti': '*', 'bool': False, 'f': ['*', 2]}, 
-'`': { 'ocb': 'b', 'mean': ['<pre><code>', '</code></pre>'], 'anti': '`', 'bool': False, 'f': ['`', 1]}, 
-'[': { 'ocb': 'o', 'mean': ['<a href="'], 'anti': ']', 'bool': False, 'f': ['[', 1]},
-']': { 'ocb': 'c', 'mean': Link_text , 'anti': '[', 'f': [']', 1]}}
+'_': {'ocb': 'b', 'mean': ['<u>', '</u>'], 'anti': '_', 'bool': False, 'num': 2}, 
+'*': {'ocb': 'b', 'mean': ['<b>', '</b>'], 'anti': '*', 'bool': False, 'num': 2}, 
+'`': { 'ocb': 'b', 'mean': ['<pre><code>', '</code></pre>'], 'anti': '`', 'bool': False, 'num': 1}, 
+'[': { 'ocb': 'o', 'mean': ['<a href="'], 'anti': ']', 'bool': False, 'num': 1},
+']': { 'ocb': 'c', 'mean': Link_text , 'anti': '[', 'num': 1}}
 
 def use_parser(el):
     if (el_d['`']['bool'] and el != '`') or (el_d['[']['bool'] and el != ']'):
@@ -33,9 +38,9 @@ def parser(text):
         something_else_you_need(text[i])
         if text[i] in el_d and use_parser(text[i]):
             el = text[i]
-            q, plus = True, el_d[el]['f'][1]
+            q, plus = True, el_d[el]['num']
             for j in range(plus):
-                if not(i + j < len(text) and text[i + j] == el_d[el]['f'][0]):
+                if not(i + j < len(text) and text[i + j] == el):
                     q = False
             if q:
                 if el_d[el]['ocb'] == 'b':
