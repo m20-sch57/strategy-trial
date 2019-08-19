af8e158cd4c8b4f83ad433ce7aabdb719f13168c
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-08-19 19:03:52 +0300

    Now testing uses su (better because user test has no password and setting PermitEmptyPassword to no in /etc/ssh/sshd_config solves the potential problem)

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,3 +1,5 @@
 chmod o+r '/home/test/problems/'$1'/strategies/'$2
-ssh -l test -E ../.strategy-trial.ssh.log localhost 'ulimit -t '$3' ; python3 shell.py || exit $?' || exit $?
+su - test -c "ulimit -t $3 ; python3 shell.py"
+retcode=$?
 chmod o-r '/home/test/problems/'$1'/strategies/'$2
+exit $retcode
