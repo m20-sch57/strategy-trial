5182eb83ac3e7062b94b0b93446cf1ae8b0078c7
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-08-25 13:31:52 +0300

    Now with memory limit of 256 MB. Suggestion: TL -> RL (Resource Limit)

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod o+r '/home/test/problems/'$1'/strategies/'$2
-su - test -c "ulimit -t $3 ; python3 shell.py"
+su - test -c "ulimit -t $3 -v 262144 ; python3 shell.py"
 retcode=$?
 chmod o-r '/home/test/problems/'$1'/strategies/'$2
 exit $retcode
