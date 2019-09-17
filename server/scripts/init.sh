afcf1fba612316a6a710bf2e97e8f8113209983b
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2019-09-17 08:39:19 +0300

    fixed a bug

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -3,5 +3,7 @@
 chgrp test problems -R
 chmod o-rwx "/home/dba/problems" -R
 chmod g-rw "/home/dba/problems" -R
-chmod g+x "/home/dba/problems" -R
+chmod g+rx "/home/dba/problems"
+chmod g+rx "/home/dba/problems/$1"
+chmod g+rx "/home/dba/problems/$1/strategies"
 chmod g+r "/home/dba/problems/$1/classes.py"
